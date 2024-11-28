f082b253ac6837c85bb4ff7095fd9db0c4b5886b
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-11-28 20:15:21 +0530

    TSA

--- swagger.js
+++ swagger.js
@@ -6,8 +6,8 @@ const doc = {
     description:
       "The Expense Tracker App is a web application for managing personal expenses. It allows users to track their daily expenses, categorize them, and analyze statistics related to their spending. The app also supports bulk upload of expenses via CSV files, and it includes an admin role for managing expenses and users.",
   },
-  schemes: ["http", "https"], 
-  basePath: "/", 
+  schemes: ["http", "https"],
+  basePath: "/",
 };
 
 const outputFile = "./swagger-output.json";
