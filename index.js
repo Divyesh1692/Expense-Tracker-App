eacc997ce6a5e512b13c6d9d8d9306cbb2712355
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-11-28 07:49:48 +0530

    ETA

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dbConnect = require("./config/db");
 const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/userRoutes");
 const expenseRouter = require("./routes/expenseRoute");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
@@ -15,8 +17,19 @@ app.use(cookieParser());
 
 app.use("/user", userRouter);
 app.use("/expenses", expenseRouter);
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    explorer: true, // Allow API exploration
+    swaggerOptions: {
+      docExpansion: "none", // Optionally, control the display of API documentation
+    },
+  })
+);
 app.get("/", (req, res) => {
-  res.send("Welcome to E-Library Management System");
+  res.send("Welcome to Expense Tracker App");
 });
 
 app.listen(PORT, () => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,9 +9,50 @@ const auth = require("../middlewares/auth");
 
 const userRouter = Router();
 
+userRouter.post("@swagger/signup", signup);
+userRouter.post("@swagger/login", login);
+userRouter.post("@swagger/logout", logout);
+userRouter.get("@swagger/allusers", auth, getAllUsers);
+
+/**
+ * @swagger
+ * /user/signup:
+ *   post:
+ *     summary: Create a new user
+ *     description: Sign up a new user with the provided credentials.
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SuccessResponse'
+ *       400:
+ *         description: Bad request - validation error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/InternalErrorResponse'
+ */
 userRouter.post("/signup", signup);
-userRouter.post("/login", login);
-userRouter.post("/logout", logout);
-userRouter.get("/allusers", auth, getAllUsers);
+
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *     summary: Login a user
+ *     description: Authenticate a user and return a JWT token for authorization.
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SuccessResponse'
+ *       400:
+ *         description: Invalid credentials
+ *         content:
+ *           application/json:
+ */
 
 module.exports = userRouter;
